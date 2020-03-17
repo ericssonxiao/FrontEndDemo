7db867da8f80b018d238530929e7ccc98d2ed80c
ericssonxiao <ericssonxiao@gmail.com>
2020-03-17 17:11:49 -0400

    first angular sample

--- angular-tutorials/first-sample/src/app/app.module.ts
+++ angular-tutorials/first-sample/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path:'', component: ProductListComponent },
+      { path:'products/:productId', component:ProductDetailComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
