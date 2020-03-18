58955e41dfbb56ced39325e714c4de37b8e208ba
ericssonxiao <ericssonxiao@gmail.com>
2020-03-17 21:07:01 -0400

    submit code

--- angular-tutorials/first-sample/src/app/app.module.ts
+++ angular-tutorials/first-sample/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +18,19 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     ToolBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { path:'', component: ProductListComponent },
-      { path:'products/:productId', component:ProductDetailComponent },
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component:ProductDetailComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   providers: [],
