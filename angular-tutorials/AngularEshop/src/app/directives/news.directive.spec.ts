20e464428bf416f20e8d251a0de3cf906d80049f
ericssonxiao <ericssonxiao@gmail.com>
2020-03-21 06:26:40 -0400

    adjust file structure

--- angular-tutorials/AngularEshop/src/app/directives/news.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewsDirective } from './news.directive';
-
-describe('NewsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NewsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
